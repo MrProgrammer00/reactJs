a1dde7bb017236d2280fe8d0164da1163b66f13c
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2023-01-04 14:45:33 +0530

    frist commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Menulist from './components/Menulist';
+import Main from './components/Main';
+function App() {
+  return (
+    <div className="App">
+      
+      <Navbar />
+      <Menulist />
+      <Main>  </Main>
+    </div>
+  );
+}
+
+export default App;
